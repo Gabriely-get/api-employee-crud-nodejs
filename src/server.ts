b22249cae840b9862d133ac7971c259cc670d7d2
Maycon Vieira <maycon-big@hotmail.com>
2023-11-29 15:39:03 -0300

    addind cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
+// importa o express
 import express from 'express';
-import Roteador from './routes';
-
+// importar rotas
+import Routes from './routes';
+// instancia o express
 const app = express();
-
+// Instancia CORS
+const cors = require('cors');
+// Configura CORS
+app.use(cors())
+// Configurar uso de json
 app.use(express.json());
-app.use(Roteador);
-
+// Configuração de uso das rotas
+app.use(Routes);
+//configura porta e funçao executada na ativação
 app.listen(4000, ()=>{console.log("Servidor Iniciado")} );
